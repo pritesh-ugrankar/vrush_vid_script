a0b7cc378ba406a7d09930a2189a790e8644d98b
pritesh-ugrankar <pritesh.manohar.ugrankar@gmail.com>
2021-03-22 22:42:15 +0530

    Updated the Perl Script with comments. Uncommented the main commandgit add perl-youtube-dl.pl

--- perl-youtube-dl.pl
+++ perl-youtube-dl.pl
@@ -7,9 +7,13 @@ open (my $fh, "<", $filename) or die "Cannot open file $filename.";
 while (<$fh>) {
 
 	chomp;
+
+	#Ignore any lines that start with a "#".
+	#This is so that we can hash out unwanted file lines or add comments.
 	next if /^#.*/;
+
 	print "Downloading: $_\n";
-	#`youtube-dl $_` or die "Could not download the file $_";
+	`youtube-dl $_` or die "Could not download the file $_";
 	print "Downloaded: $_ :successfully\n";
 	print "----" x 20, "\n";
 }
